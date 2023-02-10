d66a9479219d2b1142267ae9b7e14e1920d0d47c
kate <kat4011yandex@mail.ru>
2023-02-10 15:44:35 +0300

    First seminar enden

---
+++ Seminar1.md/Program.cs
@@ -0,0 +1,66 @@
+﻿// int num = -12;
+// double bigNum = -12.543;
+// bool isEven = true;
+// string word = "flower";
+// char symbol = '4';
+
+// int num1 = -12;
+// int num2 = 6;
+// Console.Write($"my number are " + num1 + " and " + num2 +  " and  it is good!");
+// Console.WriteLine($"my number are {num1}  and  {num2} and  it is good!");
+
+// Console.Write("input an integer number: ");
+// int num = Convert.ToInt32 (Console.ReadLine());
+
+// Console.WriteLine("your number is " +  num);
+
+// Seminar1
+// homeWork1
+
+// Task 1. Напишите программу, которая на вход принимает два числа и проверяет,
+// является ли первое число квадратом второго. 
+
+/*  Console.Write("input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int guad2 = num2 * num2;
+
+if(num1==guad2)
+{
+    Console.Write("yes");
+}
+else 
+{
+    Console.Write("no");
+}
+
+*/
+// Task2. Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показыват все цлые числа в промежетке от - N до N.
+
+/* Console.Write("input a positive number:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = -num;
+
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current ++;
+}
+*/
+
+/* 34542 / 10 = 3154
+34542 / 100 = 315
+34542 / 1000 = 31
+34542 / 10 = 3
+
+31542 % 10 =2
+31542 % 100 =42
+31542 % 1000 =542
+31542 % 10000 =1542
+
+453 -> 32
+*/ 
