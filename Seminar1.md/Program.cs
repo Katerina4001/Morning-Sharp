e9bd94880d38df00c02793d0a5780f7219fdcf05
kate <kat4011yandex@mail.ru>
2023-02-15 21:00:17 +0300

    домашнее задание

--- Seminar1.md/Program.cs
+++ Seminar1.md/Program.cs
@@ -64,3 +64,88 @@ while(current <= num)
 
 453 -> 32
 */ 
+
+/*Задача 3. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.*/
+
+ /*   
+Console.Write("введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.Write("первое число максимальное");
+}
+else 
+{
+    Console.Write("второе число максимальное");
+}
+ */
+
+/* Задача 4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел. */
+
+/*
+Console.Write("введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+
+if (num1 > num2)
+{
+   if  (num1 > num3)
+{
+    Console.WriteLine("максимальное число:  num1" );
+}
+}
+else
+{
+    if (num2 > num3)
+    {
+      Console.WriteLine("максимальное число:  num2" );  
+    }
+    else
+   {
+     Console.WriteLine("максимальное число:  num3" );
+     }
+}
+*/
+
+/* Задача 5. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка) */
+
+/* Console.Write("введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0)
+{
+    Console.Write("число четное");
+}
+else 
+{
+    Console.Write("число нечетное");
+}
+*/
+/*Задача 6.  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.*/
+
+/*Console.Write("введите число:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current  = 1;
+while (current <= num)
+{
+    if (current % 2 == 0)
+
+    {
+       Console.Write(current + " "); 
+    } 
+  
+    current ++;
+   }
+    */
+
